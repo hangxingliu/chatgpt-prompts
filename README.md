96505ba4a1edfcfaebf854d19a1442e566c7a776
Liu Yue <hangxingliu@gmail.com>
2023-10-14 17:37:34 +0800

    fix scripts for new GPT-4 input area

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A project that contains some prompt texts and a chrome extension
 
+## Chrome Extension
+
+<https://chrome.google.com/webstore/detail/chatgpt-prompts/lbkkaioifbfjklefgngfplegmjnlnfpm>
+
 ## Project Structure
 
 - **prompts**: The directory that contains prompt texts
--- chrome-extension/src/inject/input-prompt.ts
+++ chrome-extension/src/inject/input-prompt.ts
@@ -8,25 +8,35 @@ export function inject(parameters: { text: string }) {
   const { text } = parameters;
   debug(`injecting the prompt "${text.slice(0, 16)}..."`);
 
-  const _inputs = $$<HTMLTextAreaElement>("#__next textarea");
+  let allTextAreas = $$<HTMLTextAreaElement>("#__next textarea");
+  const exactMatched = allTextAreas.find(it => it.id === 'prompt-textarea');
+  if (exactMatched) allTextAreas = [exactMatched];
+
   let input: HTMLTextAreaElement;
   let button: HTMLButtonElement;
-  for (let i = 0; i < _inputs.length; i++) {
-    const _input = _inputs[i];
-    button = _input.nextElementSibling as HTMLButtonElement;
-    if (button?.tagName !== "BUTTON") continue;
+  for (let i = 0; i < allTextAreas.length; i++) {
+    const _input = allTextAreas[i];
+    button = getSendButtonFromInput(_input);
+    if (!button) continue;
     input = _input;
     break;
   }
 
   if (input) {
-    debug(`found text area (original: ${_inputs.length})`);
+    debug(`found text area (original: ${allTextAreas.length})`);
     inputPrompt(input, button).catch((error) => console.error(error));
   }
 
+  function getSendButtonFromInput(input: HTMLTextAreaElement): HTMLButtonElement {
+    let button = input.nextElementSibling as HTMLButtonElement;
+    if (button?.tagName === "BUTTON") return button
+
+    button = input.parentElement.lastElementChild as HTMLButtonElement;
+    if (button?.tagName === "BUTTON") return button;
+  }
   async function inputPrompt(input: HTMLTextAreaElement, button: HTMLButtonElement) {
     input.focus();
-    document.execCommand('insertText', false, text);
+    document.execCommand("insertText", false, text);
     button.removeAttribute("disabled");
     button.click();
   }
